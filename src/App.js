96093f197c95fc92a99ee8011d5d30cff3221c53
iwswordpress <iwswordpress@gmail.com>
2020-08-13 12:21:54 +0100

    18

--- src/App.js
+++ src/App.js
@@ -4,28 +4,41 @@ import Navbar from './components/layout/Navbar';
 import axios from 'axios';
 
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 class App extends Component {
   state = {
     users: [],
     loading: false
   };
-  async componentDidMount() {
-    console.log('Client_ID: ', process.env.REACT_APP_GITHUB_CLIENT_ID);
-    console.log('Client_SECRET: ', process.env.REACT_APP_GITHUB_CLIENT_SECRET);
+  // async componentDidMount() {
+  //   console.log('Client_ID: ', process.env.REACT_APP_GITHUB_CLIENT_ID);
+  //   console.log('Client_SECRET: ', process.env.REACT_APP_GITHUB_CLIENT_SECRET);
 
+  //   this.setState({ loading: true });
+  //   console.log('get data via async/await...');
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  //   );
+  //   this.setState({ users: res.data, loading: false });
+  //   console.log(res.data);
+  // }
+  searchUsers = async (text) => {
+    console.log('[APP] search term ', text);
     this.setState({ loading: true });
     console.log('get data via async/await...');
-    const res = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-    this.setState({ users: res.data, loading: false });
-    console.log(res.data);
-  }
+    const url = `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`;
+
+    console.log('URl: ', url);
+    const res = await axios.get(url);
+    this.setState({ users: res.data.items, loading: false });
+    console.log(res.data.items);
+  };
   render() {
     return (
       <div className='App'>
         <Navbar title='Github Finder' icon='fa fa-github' />
         <div className='container'>
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
---
+++ src/components/users/Search.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  state = {
+    text: ''
+  };
+  onChange = (e) => {
+    //console.log(e.target.value);
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  onSubmitForm = (e) => {
+    e.preventDefault();
+    console.log('SUBMIT ', this.state.text);
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: '' });
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onSubmitForm} className='form'>
+          <input
+            type='text'
+            name='text'
+            placeholder='Search users...'
+            value={this.state.text}
+            onChange={this.onChange}
+          />
+          <input
+            type='submit'
+            value='Search'
+            className='btn btn-dark btn-block'
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Search;
